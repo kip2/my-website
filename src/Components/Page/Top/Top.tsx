b4ff02063cf570be69dac95351c8efd877f785ea
kip2 <tqa1no4@gmail.com>
2024-04-16 20:45:59 +0900

    Add my image

--- src/Components/Page/Top/Top.tsx
+++ src/Components/Page/Top/Top.tsx
@@ -2,6 +2,8 @@ function Top() {
     return (
         <>
             <h1 className="text-6xl font-bold">My Top page!</h1>
+            <img className="rounded-full w-48 h-48"
+                src="public/myimage.png" alt="my image"/>
         </>
     )
 }
