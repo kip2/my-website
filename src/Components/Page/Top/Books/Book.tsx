8ae5e4553f7da934864ffde11330fe8f0fd8139c
kip2 <tqa1no4@gmail.com>
2024-04-21 18:47:12 +0900

    Adjust layout of Book page

--- src/Components/Page/Top/Books/Book.tsx
+++ src/Components/Page/Top/Books/Book.tsx
@@ -11,7 +11,7 @@ const Book: React.FC<BookProps> = ({ title, isbn, description })  => {
         <div className="flex flex-col items-center justify-center mt-12"> 
             <h1 className="text-5xl max-md:text-3xl mb-8 underline underline-offset-8">{title}</h1>
             <BookCover isbn={isbn}/>
-            <div className=" w-100 max-md:w-96 mt-5">
+            <div className=" w-160 max-lg:w-144 max-md:w-96 mt-5">
                 <p className="py-2 mx-auto text-left leading-8">
                     {description}
                 </p>
