1821cf575cddb3dcd300db2f530a155c380fe1ca
kip2 <tqa1no4@gmail.com>
2024-04-21 18:42:34 +0900

    Add book introduction

--- src/Components/Page/Top/Books/Book.tsx
+++ src/Components/Page/Top/Books/Book.tsx
@@ -8,15 +8,12 @@ interface BookProps {
 
 const Book: React.FC<BookProps> = ({ title, isbn, description })  => {
     return(
-        <div className="flex flex-col items-center justify-center"> 
+        <div className="flex flex-col items-center justify-center mt-12"> 
             <h1 className="text-5xl max-md:text-3xl mb-8 underline underline-offset-8">{title}</h1>
             <BookCover isbn={isbn}/>
             <div className=" w-100 max-md:w-96 mt-5">
                 <p className="py-2 mx-auto text-left leading-8">
                     {description}
-                    {/* プログラミングの楽しさに目覚めた本。<br/>
-                    オライリー本なのに、エイリアンが表紙だったり、漫画が随所随所で挟まれていたり、とにかく異色の本。<br/>
-                    しかし楽しい。<br/> */}
                 </p>
             </div>
         </div>
--- src/Components/Page/Top/TopBooks.tsx
+++ src/Components/Page/Top/TopBooks.tsx
@@ -5,9 +5,13 @@ function TopBooks() {
     return (
         <>
             <div className="border-t border-white my-10 w-96 mx-auto"></div>
-            <h1 className=" text-6xl mx-auto w-fit mb-12 max-md:text-4xl">
+            <h1 className=" text-6xl mx-auto w-fit mb-7 max-md:text-4xl">
                 影響を受けた本
             </h1>
+            <div className="font-thin text-center text-xl mx-auto w-80 mb-2 italic">
+                <p>Books are the mirrors of the soul.</p>
+                <p className="flex justify-end">Virginia Woolf</p>
+            </div>
             <div>
                 {booksData.map((book, index) => (
                     <Book key={index} title={book.title} isbn={book.isbn} description={book.description} />
