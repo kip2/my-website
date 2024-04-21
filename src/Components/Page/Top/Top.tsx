b00f124c41dc5427f5968cb91a06e1a7056d4c8d
kip2 <tqa1no4@gmail.com>
2024-04-21 13:38:36 +0900

    Create profile section on the top page

--- src/Components/Page/Top/Top.tsx
+++ src/Components/Page/Top/Top.tsx
@@ -1,18 +1,44 @@
+import { faGithub, faXTwitter } from "@fortawesome/free-brands-svg-icons"
 import Blank from "../../UI/Blank"
+import Footer from "../../UI/Footer"
+import Header from "../../UI/Header"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function Top() {
     return (
         <>
+            <Header/>
             <Blank />
-            <h1 className="text-6xl font-bold w-full text-center">My Top page!</h1>
-            <Blank />
-            <div className="flex flex-col md:flex-row items-center justify-center min-h-screen">
-                <div className="w-1/2 md:w-auto max-w-md mx-4 my-2">
-                    <img className="rounded-full w-48 h-48"
-                            src="public/myimage.png" alt="my image"/>
+            <div className="mx-auto flex justify-center gap-16 max-lg:flex-col max-lg:items-center">
+                <div className="grid place-content-center">
+                    <img className="rounded-full w-60 h-60"
+                            src="myimage.png" alt="my image"/>
                 </div>
-                <p className="text-center mx-4 my-2">Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust</p>
+                <div className="flex flex-col w-96">
+                    <h2 className=" text-4xl pb-2">kip2 (@kip2)</h2>
+                    <p className=" py-2 mx-auto text-left">
+                    プログラミングに魅せられてしまい沼に沈んだオタク<br/>
+                    なんでもすぐに手を出すから何にも身についていない<br/>
+                    vimとコンソールとパソカタと、プログラミング言語と名の付くものならなんでも好き<br/>
+                    生涯をかけてコンピュータとお友達になりたい<br/>
+                    </p>
+                    <p className="pt-2 text-xl">特筆して憧れている言語(2024年時点)</p>
+                    <p>
+                    Rust<br/>
+                    Lisp系言語(Common Lisp, Scheme, Gauche, Clojure)<br/>
+                    関数型系統の言語(F#, Haskell, Scala, etc...)<br/>
+                    </p>
+                    <div className="pt-5">
+                            <a href="https://github.com/kip2" className="text-4xl mx-1 px-2 border-2 border-gray-700 rounded-xl hover:text-blue-300">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </a>
+                            <a href="https://twitter.com/kip2_ret_0" className="text-4xl px-2 mx-1 border-2 border-gray-700 rounded-xl hover:text-blue-300">
+                                <FontAwesomeIcon icon={faXTwitter} />
+                            </a>
+                    </div>
+                </div>
             </div>
+            <Footer/>
         </>
     )
 }
--- src/Components/UI/Footer.tsx
+++ src/Components/UI/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <>
             <footer className="pt-10 pb-3 flex justify-center">
-                <p className="text-lg">© 2024 kip2 All rights reserved.</p>
+                <p className="text-lg">© 2024 kip2</p>
             </footer>
         </>
     )
