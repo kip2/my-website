648d79fce2cf1e0343334f777bd504da538d407c
kip2 <tqa1no4@gmail.com>
2024-04-23 22:18:14 +0900

    Adjust margins

--- src/Components/Page/About/Qualifications.tsx
+++ src/Components/Page/About/Qualifications.tsx
@@ -5,7 +5,7 @@ const Qualifications = () => {
     return (
         <>
             <div className="border-t border-white mb-20 mt-7 w-96 mx-auto"></div>
-            <h1 className=" text-6xl mx-auto w-fit mb-7 max-md:text-4xl">
+            <h1 className=" text-6xl mx-auto w-fit mb-14 max-md:text-4xl">
                 Qualifications
             </h1>
             {qualificationData.map((qualification, index) => (
