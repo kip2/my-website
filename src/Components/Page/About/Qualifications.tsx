596c31e994cbf4329b6e44bb26e758eb03ed3a76
kip2 <tqa1no4@gmail.com>
2024-04-23 21:38:06 +0900

    Create Qualification component

--- src/Components/Page/About/Qualifications.tsx
+++ src/Components/Page/About/Qualifications.tsx
@@ -1,7 +1,19 @@
 const Quolifications = () => {
     return (
         <>
-            <h1>Quolifications</h1>
+            <div className="border-t border-white mb-20 mt-7 w-96 mx-auto"></div>
+            <h1 className=" text-6xl mx-auto w-fit mb-7 max-md:text-4xl">
+                Quolifications
+            </h1>
+            <section className="flex justify-center">
+                <div className=" grid grid-cols-3 w-128 text-2xl border-2 rounded-md px-5 py-2">
+                    <div className="flex items-center">
+                        <p>🏆</p>
+                        <p className="italic ml-2 text-sm text-gray-400">2023/03/16</p>
+                    </div>
+                    <p className="italic">LPIC-1</p>
+                </div>
+            </section>
         </>
     )
 }
