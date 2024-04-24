7a31b54380728e86c585fbe5751a1bd364e59cfc
kip2 <tqa1no4@gmail.com>
2024-04-25 07:23:30 +0900

    Add Header and Footer in Products page

--- src/Components/Page/Products/Products.tsx
+++ src/Components/Page/Products/Products.tsx
@@ -1,7 +1,12 @@
+import Footer from "../../UI/Footer"
+import Header from "../../UI/Header"
+
 function Products() {
     return(
         <>
+            <Header />
             <h1>My Products!</h1>
+            <Footer />
         </>
     )
 }
