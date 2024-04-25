deed9aba53c6d6c00f03468e1b508760e0018f24
kip2 <tqa1no4@gmail.com>
2024-04-25 21:58:15 +0900

    Add routing to 404 page

---
+++ src/Components/Page/Error/NotFoundPage.tsx
@@ -0,0 +1,14 @@
+import Blank from "../../UI/Blank"
+
+const NotFoundPage = () => {
+    return (
+        <section className="my-auto mx-auto text-6xl italic grid justify-items-center">
+            <Blank/>
+            <Blank/>
+            <h1>404</h1>
+            <h1>Not Found</h1>
+        </section>
+    )
+}
+
+export default NotFoundPage
