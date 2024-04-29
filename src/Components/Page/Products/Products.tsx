3c72da62a0330f234a172bc55e4b5ce34a03348f
kip2 <tqa1no4@gmail.com>
2024-04-29 15:30:28 +0900

    Add functionality to read and ouput markdown files

--- src/Components/Page/Products/Products.tsx
+++ src/Components/Page/Products/Products.tsx
@@ -1,11 +1,17 @@
+import Blank from "../../UI/Blank"
 import Footer from "../../UI/Footer"
 import Header from "../../UI/Header"
+import MarkdownViewer from "./MarkdownViewer"
 
 function Products() {
     return(
         <>
             <Header />
             <h1>My Products!</h1>
+            <Blank />
+            <div className=" text-xl mx-auto w-fit mb-7 max-md:text-4xl">
+                <MarkdownViewer filepath="/src/markdown/Products/sample.md" />
+            </div>
             <Footer />
         </>
     )
