3a2e638761d290dfac877ed23f417fd37519a282
kip2 <tqa1no4@gmail.com>
2024-04-29 11:58:07 +0900

    Adjust layout of Qulification page

--- src/Components/Page/About/Qualification.tsx
+++ src/Components/Page/About/Qualification.tsx
@@ -8,7 +8,12 @@ const Qualification: React.FC<QualificationProps> = ({ data, qualification, url}
     return (
         <>
             <section className="flex justify-center mt-5">
-                <div className=" grid max-md:grid-cols-2 grid-cols-3 w-144 max-md:w-128 max-md:text-base text-2xl border-2 bg-gray-400/10  border-gray-700 rounded-md px-6 py-3 hover:bg-gray-600 transition duration-300 ease-in-out">
+                <div className="grid max-md:grid-cols-2 grid-cols-3 w-190 max-lg:w-144 max-md:w-128 max-sm:w-96
+                    text-2xl 
+                    border-2 bg-gray-400/10  border-gray-700 rounded-md 
+                    px-6 py-3 
+                    hover:bg-gray-600 transition duration-300 ease-in-out
+                ">
                     <a href={url} className="flex items-center">
                         <p>🏆</p>
                         <p className="italic ml-2 max-md:text-xs text-sm text-gray-400">{data}</p>
