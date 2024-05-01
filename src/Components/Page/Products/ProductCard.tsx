c64f77d3c785ef3baf1a6964cd665876d232dcb6
kip2 <tqa1no4@gmail.com>
2024-05-01 21:07:30 +0900

    Create skelton of card layout

--- src/Components/Page/Products/ProductCard.tsx
+++ src/Components/Page/Products/ProductCard.tsx
@@ -1,7 +1,22 @@
 const ProductCard = () => {
     return (
         <>
-            <h1>Card</h1>
+            <div className="w-80 h-50 rounded-md border-2 border-slate-500 overflow-hidden 
+                    hover:bg-slate-200/10 duration-500 hover:border-slate-200
+                ">
+                <div className="h-2/3">
+                    <img className="w-full h-full object-cover" src="/img/thumbnail/yume_t.png"></img>
+                </div>
+                <div className="h-1/2 flex flex-col p-4 bg-slate-300/10 ">
+                    <h2 className="mx-auto text-lg font-semibold pb-2 overflow-hidden">
+                        せっかち夢十夜
+                    </h2>
+                    <p className="mx-auto text-sm overflow-hidden whitespace-nowrap">
+                        {/* 40文字のみ */}
+                        夢十夜を元ネタにしたClickerゲーム
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
