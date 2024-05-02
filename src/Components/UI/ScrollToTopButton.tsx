5bb03da789b952b656d5b964624e3e3b2b484844
kip2 <tqa1no4@gmail.com>
2024-05-02 22:58:24 +0900

    Adjust size of ScrollToTopButton

--- src/Components/UI/ScrollToTopButton.tsx
+++ src/Components/UI/ScrollToTopButton.tsx
@@ -26,8 +26,7 @@ const ScrollToTopButton = () => {
         <>
             <button onClick={scrollToTop} disabled={isTop}
             className=' border-2 rounded-full
-             border-zinc-400
-             bg-indigo-900
+                border-zinc-400 bg-indigo-900
                 bottom-5 right-5 z-50 fixed text-lg
                 py-2 px-5
                 shadow opacity-50
@@ -37,7 +36,7 @@ const ScrollToTopButton = () => {
                 opacity: isTop ? 0.3 : 0.8,
             }}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" 
-                    width={40} height={40} >
+                    width={32} height={32} >
                     <rect width="256" height="256" fill="none"/>
                     <polyline points="48 160 128 80 208 160" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
                 </svg>
