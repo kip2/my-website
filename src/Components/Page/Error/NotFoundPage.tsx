3bfe879a09f5c3118bdb57557704b25b078a4533
kip2 <tqa1no4@gmail.com>
2024-05-02 18:33:37 +0900

    Add ReturnButton to top page on the 404 page

--- src/Components/Page/Error/NotFoundPage.tsx
+++ src/Components/Page/Error/NotFoundPage.tsx
@@ -1,4 +1,5 @@
 import Blank from "../../UI/Blank"
+import ReturnButton from "../../UI/ReturnButton"
 
 const NotFoundPage = () => {
     return (
@@ -7,6 +8,10 @@ const NotFoundPage = () => {
             <Blank/>
             <h1>404</h1>
             <h1>Not Found</h1>
+            <Blank />
+            <Blank />
+            <ReturnButton path="/"/>
+            <p className="mt-5 text-lg">return to top page...</p>
         </section>
     )
 }
