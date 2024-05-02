4a54e018870b50b6fb9ee4d03be199836b2e351d
kip2 <tqa1no4@gmail.com>
2024-05-02 18:50:21 +0900

    Change confitions for displaying lines

--- src/Components/Page/Products/ProductCards.tsx
+++ src/Components/Page/Products/ProductCards.tsx
@@ -10,7 +10,7 @@ const ProductCards = () => {
                 max-sm:grid-cols-1 max-sm:justify-items-center
             ">
                 {producttData.map((data, index) => (
-                    index % 6 === 0 ?
+                    (index % 6 === 0) && (index !== 0)  ?
                         <>
                             <div className=" col-span-3 border-t border-white mb-7 mt-7 w-96 mx-auto max-md:col-span-2 max-sm:hidden"></div>
                             <ProductCard
