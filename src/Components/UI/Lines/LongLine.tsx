be486fe8862fbb3681dfaed1d14dbb0c825fe7c1
kip2 <tqa1no4@gmail.com>
2024-05-03 01:57:57 +0900

    Add LingLine component and change directory structure

---
+++ src/Components/UI/Lines/LongLine.tsx
@@ -0,0 +1,9 @@
+const LongLine = () => {
+    return (
+        <>
+            <div className="border-t border-white mb-16 mt-7 w-210 mx-auto"></div>
+        </>
+    )
+}
+
+export default LongLine
