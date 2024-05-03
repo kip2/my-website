faeb26da0cdb11bce5365a2a78960fdd00c7dafb
kip2 <tqa1no4@gmail.com>
2024-05-03 12:20:58 +0900

    Adjust width of LongLine

--- src/Components/UI/Lines/LongLine.tsx
+++ src/Components/UI/Lines/LongLine.tsx
@@ -1,7 +1,7 @@
 const LongLine = () => {
     return (
         <>
-            <div className="border-t border-white mb-2  mt-2 w-160 mx-auto"></div>
+            <div className="border-t border-white mb-2  mt-2 w-160 mx-auto max-md:w-144 max-sm:w-96 max-[470px]:w-72 "></div>
         </>
     )
 }
