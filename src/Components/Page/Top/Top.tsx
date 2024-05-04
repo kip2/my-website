c141966dc38fa7332a7f71374d5dc85f41b14f04
kip2 <tqa1no4@gmail.com>
2024-05-05 00:25:06 +0900

    Adjust layout of Top page

--- src/Components/Page/Top/Top.tsx
+++ src/Components/Page/Top/Top.tsx
@@ -1,20 +1,17 @@
 import Blank from "../../UI/Blanks/Blank"
-import LittleBlank from "../../UI/Blanks/LittleBlank"
 import Footer from "../../UI/Footer"
 import Header from "../../UI/Header"
 import Quotes from "../../UI/Quotes"
 import ScrollToTopButton from "../../UI/ScrollToTopButton"
-import UnderConstruction from "../../UI/UnderConstruction"
 import Profile from "./Profile"
 
 const Top = () => {
     return (
         <>
-            <UnderConstruction />
             <Header/>
             <Blank />
             <Quotes />
-            <LittleBlank />
+            <Blank />
 
             <Profile />
 
