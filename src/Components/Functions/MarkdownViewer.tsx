be65d4fa0d1b0c834946ba03834ef11072842b8a
kip2 <tqa1no4@gmail.com>
2024-05-04 10:13:21 +0900

    Change markdown parser to react-markdown

--- src/Components/Functions/MarkdownViewer.tsx
+++ src/Components/Functions/MarkdownViewer.tsx
@@ -1,26 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { marked } from 'marked';
-import DOMPurify from "dompurify";
-import parse, {Element} from 'html-react-parser';
+import ReactMarkdown, {Components} from 'react-markdown';
+import gfm from 'remark-gfm';
+import DOMPurify from 'dompurify';
+import { useNavigate } from 'react-router-dom';
 import Player from '../UI/Player';
-import "./MarkdownViewer.css"
-import {  useNavigate } from 'react-router-dom';
+import "./MarkdownViewer.css";
 
 interface MarkdownViewerProps {
     filepath: string;
 }
 
 const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ filepath }) => {
-    const [content, setContent] = useState<React.ReactNode>(null);
-    const navigate = useNavigate()
+    const [markdown, setMarkdown] = useState<string>('');
+    const navigate = useNavigate();
 
     useEffect(() => {
-
-        marked.setOptions({
-            breaks: true,
-            gfm: true
-        })
-
         fetch(filepath)
             .then(response => {
                 if (!response.ok) {
@@ -30,67 +24,34 @@ const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ filepath }) => {
             })
             .then(text => {
                 if (text.includes('<title>kip2.dev</title>')){
-                    navigate('/404', { replace: true })
-                    return null
+                    navigate('/404', { replace: true });
+                    return;
                 }
-                return marked(text)})
-            .then(markdown => {
-                if (!markdown) return null
-                return DOMPurify.sanitize(markdown)
-            })
-            .then(sanitizedHtml => {
-                if (!sanitizedHtml) return 
-
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(sanitizedHtml, 'text/html');
-
-                doc.querySelectorAll("p a[href*='youtube.com'], p a[href*='youtu.be']").forEach(a => {
-                    const p = a.parentElement;
-                    if (p?.tagName === 'P') {
-                        const div = doc.createElement('div');
-                        while (p.firstChild) {
-                            div.appendChild(p.firstChild);
-                        }
-                        p.parentNode?.replaceChild(div, p);
-                    }
-                });
-
-                const newHtml = doc.body.innerHTML;
-                return newHtml;
-            })
-            .then(sanitizedHtml => {
-                if (!sanitizedHtml) return
-
-                const transformedContent = parse(sanitizedHtml, {
-                    replace: (domNode) => {
-                        if (domNode instanceof Element && domNode.name === 'a') {
-                            const href = domNode.attribs.href;
-                            const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]+)/i;
-                            const match = href.match(youtubeRegex);
-                            if (match) {
-                                return <Player url={href} />;
-                            }
-                        }
-                    }
-                });
-
-                setContent(transformedContent);
+                const sanitized = DOMPurify.sanitize(text);
+                setMarkdown(sanitized);
             })
             .catch(error => {
                 console.error("Markdown file could not be loaded or parsed", error);
-                setContent(<p>Error loading content...</p>);
+                setMarkdown("Error loading content...");
             });
     }, [filepath, navigate]);
 
+    const renderers: Partial<Components> = {
+        a: ({ ...props}) => { 
+            const href = props.href || ''
+            const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]+)/i
+            if (href.match(youtubeRegex)) {
+                return <Player url={href} />;
+            }
+            return <a {...props}>{props.children}</a>;
+        }
+    };
 
     return (
-        <div className=" text-xl mx-auto w-fit mb-7 max-md:text-4xl">
-            <div className="markdown-viewer">
-                {content}
-            </div>
+        <div className="markdown-viewer text-xl mx-auto w-fit mb-7 max-md:text-4xl">
+            <ReactMarkdown remarkPlugins={[gfm]} children={markdown} components={renderers} />
         </div>
     );
 };
 
 export default MarkdownViewer;
-
