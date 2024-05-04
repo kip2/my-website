96bf18e2d0e4d322faaa454bc31aaf25beeb991d
kip2 <tqa1no4@gmail.com>
2024-05-04 15:18:26 +0900

    Implement sorting by date

--- src/Components/Page/Products/ProductCards.tsx
+++ src/Components/Page/Products/ProductCards.tsx
@@ -10,14 +10,20 @@ const ProductCards:React.FC<ProductCardsProps> = ({ genre }) => {
 
     const filterProducts = producttData.filter(product => product.genre === genre)
 
+    const sortedProducts = filterProducts.sort((a, b) => {
+        const dataA = new Date(a.created).getTime()
+        const dataB = new Date(b.created).getTime()
+        return dataB - dataA
+    })
+
     return (
         <>
             <div className="grid gap-4 grid-cols-3 mx-auto max-w-6xl 
                 max-md:grid-cols-2 
                 max-sm:grid-cols-1 max-sm:justify-items-center
             ">
-                {filterProducts.length > 0 ? (
-                    filterProducts.map((data, index) => (
+                {sortedProducts.length > 0 ? (
+                    sortedProducts.map((data, index) => (
                         (index % 6 === 0) && (index !== 0)  ?
                             <React.Fragment key={index}>
                                 <div className=" col-span-3 border-t border-white mb-7 mt-7 w-96 mx-auto max-md:col-span-2 max-sm:hidden"></div>
