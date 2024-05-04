fcfa821b318205defc1368b208a007819a1a2248
kip2 <tqa1no4@gmail.com>
2024-05-04 22:15:42 +0900

    Complete creation of introduction section

--- src/Components/Page/About/Introduction.tsx
+++ src/Components/Page/About/Introduction.tsx
@@ -1,14 +1,13 @@
+import MarkdownViewer from "../../Functions/MarkdownViewer/MarkdownViewer"
 import Title from "../../UI/Title"
 
 const Introduction = () => {
+    const filepath = "/pages/About/introducion.md"
+
     return (
         <>
             <Title title="Introduction" subTitle="自己紹介" linkId="Introduction" />
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi consequatur explicabo excepturi ea, quidem dolores molestiae architecto sit nulla quos fugit ad possimus asperiores, rerum hic saepe ipsum consectetur deleniti.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi consequatur explicabo excepturi ea, quidem dolores molestiae architecto sit nulla quos fugit ad possimus asperiores, rerum hic saepe ipsum consectetur deleniti.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi consequatur explicabo excepturi ea, quidem dolores molestiae architecto sit nulla quos fugit ad possimus asperiores, rerum hic saepe ipsum consectetur deleniti.
-            </p>
+            <MarkdownViewer filepath={filepath}/>
         </>
     )
 }
