ce6bc34a3113cce06a5040737ec01b34158e2132
kip2 <tqa1no4@gmail.com>
2024-05-05 16:40:43 +0900

    Adjust layout of SeeAllPageLinkCard

--- src/Components/UI/SeeAllPageLinkCard.tsx
+++ src/Components/UI/SeeAllPageLinkCard.tsx
@@ -9,7 +9,7 @@ interface SeeAllPageLinkCardPProps {
 const SeeAllPageLinkCard:React.FC<SeeAllPageLinkCardPProps> = ({ url, title, subtitle }) => {
     return (
         <div className="bg-slate-400/10">
-            <Link to={url} className=" h-80 rounded-md border-2 border-slate-500 overflow-hidden
+            <Link to={url} className=" h-60 rounded-md border-2 border-slate-500 overflow-hidden
                     hover:bg-slate-200/10 duration-500 hover:border-slate-200
                     max-sm:max-w-[368px] max-sm:h-32
                     flex flex-col items-center  justify-center
