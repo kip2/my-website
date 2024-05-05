9d76bc82a50a51028a9d39d783d4efe30c906cee
kip2 <tqa1no4@gmail.com>
2024-05-05 19:19:28 +0900

    Change layout of Introduction to hardcoded to prevent layout issues

--- src/Components/Page/About/Introduction/Introduction.tsx
+++ src/Components/Page/About/Introduction/Introduction.tsx
@@ -1,17 +1,17 @@
-import MarkdownViewer from "../../../Functions/MarkdownViewer/MarkdownViewer"
 import Blank from "../../../UI/Blanks/Blank"
 import Title from "../../../UI/Title"
 import IntroductionProfile from "./IntroductionProfile"
+import "../../../Functions/MarkdownViewer/MarkdownViewer.css"
+import IntroductionSentense from "./IntroductionSentence"
 
 const Introduction = () => {
-    const filepath = "/pages/About/introducion.md"
 
     return (
         <>
             <Title title="Introduction" subTitle="自己紹介" linkId="Introduction" />
             <IntroductionProfile />
             <Blank />
-            <MarkdownViewer filepath={filepath}/>
+            <IntroductionSentense/>
         </>
     )
 }
