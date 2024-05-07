85bf908d110385d937aecdda5ac3777672772080
kip2 <tqa1no4@gmail.com>
2024-05-07 20:15:41 +0900

    Adjust layout of Footer

--- src/Components/UI/Footer.tsx
+++ src/Components/UI/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
         <>
-            <div className="border-t border-white my-10"></div>
+            <div className="border-t border-white mt-10 mb-7"></div>
             <footer className="pb-3 flex justify-center">
-                <p className="text-lg font-serif font-bold mb-1">© 2024 kip2</p>
+                <p className="text-lg font-serif font-bold mb-4">© 2024 kip2</p>
             </footer>
         </>
     )
