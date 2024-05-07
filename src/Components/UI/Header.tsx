6c3ea3ab67d9c0d0e5914dee046c0a768ae51bd9
kip2 <tqa1no4@gmail.com>
2024-05-07 20:24:42 +0900

    Add Favorite button and Influenced button

--- src/Components/UI/Header.tsx
+++ src/Components/UI/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
                         kip2.dev
                     </div>
                 </Link>
-                <div className="grid grid-cols-3 gap-4">
+                <div className="grid grid-cols-5 gap-4">
                     <Link to="/" className="grid justify-items-center text-xs 
                         hover:text-black hover:bg-slate-200 duration-300
                         rounded-md
@@ -51,6 +51,33 @@ const Header = () => {
                             <path d="M168,224H56a8,8,0,0,1-8-8V72a8,8,0,0,1,8-8h80l40,40V216A8,8,0,0,1,168,224Z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/><path d="M80,64V40a8,8,0,0,1,8-8h80l40,40V184a8,8,0,0,1-8,8H176" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/><line x1="88" y1="152" x2="136" y2="152" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/><line x1="88" y1="184" x2="136" y2="184" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
                         </svg>
                         <p>PRODUCTS</p>
+                    </Link>
+                    <Link to="/favorite" className="grid justify-items-center text-xs 
+                        hover:text-black hover:bg-slate-200 duration-300
+                        rounded-md
+                        px-1
+                    ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="40" height="40">
+                            <rect width="256" height="256" fill="none"/>
+                            <path d="M192,224l-64-40L64,224V48a8,8,0,0,1,8-8H184a8,8,0,0,1,8,8Z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
+                            <polyline points="192 176 127.99 136 64 176" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
+                        </svg>
+                        <p>FAVORITE</p>
+                    </Link>
+                    <Link to="/influenced" className="grid justify-items-center text-xs 
+                        hover:text-black hover:bg-slate-200 duration-300
+                        rounded-md
+                        px-1
+                    ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width="40" height="40">
+                            <rect width="256" height="256" fill="none"/>
+                            <path d="M84.27,171.73l-55.09-20.3a7.92,7.92,0,0,1,0-14.86l55.09-20.3,20.3-55.09a7.92,7.92,0,0,1,14.86,0l20.3,55.09,55.09,20.3a7.92,7.92,0,0,1,0,14.86l-55.09,20.3-20.3,55.09a7.92,7.92,0,0,1-14.86,0Z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
+                            <line x1="176" y1="16" x2="176" y2="64" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
+                            <line x1="224" y1="72" x2="224" y2="104" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
+                            <line x1="152" y1="40" x2="200" y2="40" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
+                            <line x1="208" y1="88" x2="240" y2="88" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"/>
+                        </svg>
+                        <p>INFLUENCED</p>
                     </Link>
                 </div>
             </section>
