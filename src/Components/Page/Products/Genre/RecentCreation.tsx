70047f7158537fd32e036430fd1d4aaf8ffed792
kip2 <tqa1no4@gmail.com>
2024-05-07 20:08:29 +0900

    Adjust layout of RecentCreation

--- src/Components/Page/Products/Genre/RecentCreation.tsx
+++ src/Components/Page/Products/Genre/RecentCreation.tsx
@@ -1,10 +1,12 @@
-import Title from "../../../UI/Title/Title"
+import LittleBlank from "../../../UI/Blanks/LittleBlank"
+import SubTitle from "../../../UI/Title/SubTitle"
 import ProductCards from "../ProductCards"
 
 const RecentCreation = () => {
     return (
         <>
-            <Title title="Recent" subTitle="最近つくったもの" linkId="Recenct" />
+            <SubTitle title="Recent Creation" subTitle="最近つくったもの" linkId="Recenct" />
+            <LittleBlank />
             <ProductCards limit={3}/>
         </>
     )
