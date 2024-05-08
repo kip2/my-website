b884e7bd95fd8fb84079ee63b58898c471cd537e
kip2 <tqa1no4@gmail.com>
2024-05-08 22:13:15 +0900

    Change accordion menu to the left side of the screen

--- src/Components/UI/Header/Header.tsx
+++ src/Components/UI/Header/Header.tsx
@@ -6,6 +6,7 @@ import "./Header.css"
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
+    
     return (
         <>
             <section className="flex justify-between max-md:justify-center pt-5 pb-3">
@@ -19,7 +20,7 @@ const Header = () => {
                         kip2.dev
                     </div>
                 </Link>
-                <button onClick={() => setIsOpen(!isOpen)} className={`md:hidden fixed right-7 top-4 transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}>
+                <button onClick={() => setIsOpen(!isOpen)} className={`md:hidden fixed left-7 top-4 transition-transform duration-300 ${isOpen ? 'rotate-180' : 'rotate-0'}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width={40} height={40}>
                         <rect width="256" height="256" fill="none"/>
                         <line x1="40" y1="128" x2="216" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/>
@@ -27,7 +28,7 @@ const Header = () => {
                         <line x1="40" y1="192" x2="216" y2="192" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/>
                     </svg>
                 </button>
-                <div className={`fixed top-0 right-0 h-full bg-slate-900 opacity-85 shadow-md px-3 transition-transform duration-300 z-10 ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
+                <div className={`fixed top-0 left-0 h-full bg-slate-900 opacity-85 shadow-md px-3 transition-transform duration-300 z-10 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <div className="flex justify-center my-5">
                         <div onClick={() => setIsOpen(false)} className="hover:text-black hover:bg-slate-200 duration-500 px-10 py-1 rounded-md">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width={30} height={30}>
