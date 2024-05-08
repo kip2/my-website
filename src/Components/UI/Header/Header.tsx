91f4e03aa4d2de39ed24d6fd6fe648948115b672
kip2 <tqa1no4@gmail.com>
2024-05-08 22:52:09 +0900

    Add overlay to modalize the accordion menu when opened

--- src/Components/UI/Header/Header.tsx
+++ src/Components/UI/Header/Header.tsx
@@ -9,6 +9,9 @@ const Header = () => {
     
     return (
         <>
+            {isOpen && (
+                <div className="overlay" onClick={() => setIsOpen(false)}></div>
+            )}
             <section className="flex justify-between max-md:justify-center pt-5 pb-3">
                 <Link to="/" className="my-auto">
                     <div className="logo
@@ -28,7 +31,7 @@ const Header = () => {
                         <line x1="40" y1="192" x2="216" y2="192" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/>
                     </svg>
                 </button>
-                <div className={`fixed top-0 left-0 h-full bg-slate-900 opacity-85 shadow-md px-3 transition-transform duration-300 z-10 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
+                <div className={`fixed-menu fixed top-0 left-0 h-full bg-slate-900 opacity-85 shadow-md px-3 transition-transform duration-300  ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <div className="flex justify-center my-5">
                         <div onClick={() => setIsOpen(false)} className="hover:text-black hover:bg-slate-200 duration-500 px-10 py-1 rounded-md">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" width={30} height={30}>
