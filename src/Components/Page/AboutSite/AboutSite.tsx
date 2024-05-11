7e0dfff93328d34acbc74ce0fa4fb91e2ce82af9
kip2 <tqa1no4@gmail.com>
2024-05-11 23:58:53 +0900

    Add header and footer to AboutSite page

--- src/Components/Page/AboutSite/AboutSite.tsx
+++ src/Components/Page/AboutSite/AboutSite.tsx
@@ -1,7 +1,19 @@
+import Blank from "../../UI/Blanks/Blank"
+import Footer from "../../UI/Footer"
+import Header from "../../UI/Header/Header"
+import ScrollToTopButton from "../../UI/ScrollToTopButton"
+
 const AboutSite = () => {
     return (
         <>
+            <Header />
+
+            <section>
+                <Blank />
+            </section>
             <h1>about site</h1>
+            <ScrollToTopButton />
+            <Footer />
         </>
     )
 }
