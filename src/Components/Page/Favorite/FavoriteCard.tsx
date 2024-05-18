88fd2a197c99c88f82a4930ea4fbe8eff7964aa4
kip2 <tqa1no4@gmail.com>
2024-05-18 21:36:26 +0900

    Adjust layout card of Favorite and Influenced

--- src/Components/Page/Favorite/FavoriteCard.tsx
+++ src/Components/Page/Favorite/FavoriteCard.tsx
@@ -5,20 +5,21 @@ const FavoriteCard: React.FC<{data: FavoritePageData}> = ({ data }) => {
 
     return (
         <>
-            <Link to={`/favorite/${data.id}`} className=" h-60 rounded-md border-2 border-slate-500 overflow-hidden 
+            <Link to={`/favorite/${data.id}`} className=" h-40 rounded-md border-2 border-slate-500 overflow-hidden 
                     hover:bg-slate-200/10 duration-500 hover:border-slate-200
-                    max-sm:max-w-[368px]
+                    max-sm:max-w-[368px] max-sm:min-w-[368px]
+                    max-[390px]:min-w-[260px]
                 ">
-                <div className="h-1/2  overflow-clip">
-                    <img className="h-full w-full object-cover overflow-hidden" src={data.imgUrl}></img>
-                </div>
-                <div className="h-1/2 flex flex-col items-center px-3 pt-3 pb-4 bg-slate-300/10
+                <div className="h-full flex flex-col items-center px-3 pt-3 pb-4 bg-slate-300/10
                     max-[380px]:pb-2 max-[380px]:pt-2
                 ">
-                    <p className="text-lg font-bold font-serif mb-2">
+                    <p className="h-1/2 text-lg font-bold font-serif mb-2
+                        my-2 mx-2
+                        flex items-center
+                    ">
                         {data.title}
                     </p>
-                    <p className=" text-xs font-bold">
+                    <p className="h-1/2 text-xs font-bold mx-1">
                         {data.caption}
                     </p>
                 </div>
--- src/Components/UI/SeeAllPageLinkCard.tsx
+++ src/Components/UI/SeeAllPageLinkCard.tsx
@@ -9,9 +9,10 @@ interface SeeAllPageLinkCardPProps {
 const SeeAllPageLinkCard:React.FC<SeeAllPageLinkCardPProps> = ({ url, title, subtitle }) => {
     return (
         <div className="bg-slate-400/10">
-            <Link to={url} className=" h-60 rounded-md border-2 border-slate-500 overflow-hidden
+            <Link to={url} className=" h-40 rounded-md border-2 border-slate-500 overflow-hidden
                     hover:bg-slate-200/10 duration-500 hover:border-slate-200
-                    max-sm:max-w-[368px] max-sm:h-32
+                    max-sm:max-w-[368px] max-sm:h-32 max-sm:min-w-[368px]
+                    max-[390px]:min-w-[260px]
                     flex flex-col items-center  justify-center
                     w-full
                 ">
