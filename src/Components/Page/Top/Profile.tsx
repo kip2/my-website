6a3261ae22db35f4526fa4193059d9c36d111a6b
kip2 <104058359+kip2@users.noreply.github.com>
2024-05-20 12:12:01 +0900

    Update link of X

--- src/Components/Page/Top/Profile.tsx
+++ src/Components/Page/Top/Profile.tsx
@@ -33,7 +33,7 @@ const Profile = () => {
                             ">
                                 <FontAwesomeIcon icon={faGithub} />
                             </a>
-                            <a href="https://twitter.com/kip2_ret_0" className="text-4xl px-2 mx-1 border-2 border-gray-700 rounded-xl 
+                            <a href="https://x.com/__kip2" className="text-4xl px-2 mx-1 border-2 border-gray-700 rounded-xl 
                                 hover:text-black hover:bg-slate-100 duration-300
                             ">
                                 <FontAwesomeIcon icon={faXTwitter} />
