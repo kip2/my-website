7617e721f2431cf3cbd3a85f5d926ab8976d226e
kip2 <tqa1no4@gmail.com>
2024-05-25 18:48:35 +0900

    Add OGP tags

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,36 @@ import Influenced from './Components/Page/Influenced/Influenced'
 import FavoriteDetail from './Components/Page/Favorite/FavoriteDetail'
 import InfluencedDetail from './Components/Page/Influenced/InfluencedDetail'
 import AboutSite from './Components/Page/AboutSite/AboutSite'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
 
 const App = () => {
   return (
-    <div className=' bg-neutral-800 min-h-screen md:h-auto text-white max-w-2x1 px-10 m-auto box-inherit block'>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Top />}/>
-          <Route path="/products" element={<Products />}/>
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/about" element={<About />}/>
-          <Route path="/about/history/:id" element={<HistoryDetail />} />
-          <Route path="/favorite" element={<Favorite />}/>
-          <Route path="/favorite/:id" element={<FavoriteDetail />}/>
-          <Route path="/influenced" element={<Influenced />}/>
-          <Route path="/influenced/:id" element={<InfluencedDetail />}/>
-          <Route path="/about-site" element={<AboutSite />}/>
-          <Route path="*" element={<NotFoundPage />}/>
-        </Routes>
-      </Router>
-    </div>
+    <HelmetProvider>
+      <div className=' bg-neutral-800 min-h-screen md:h-auto text-white max-w-2x1 px-10 m-auto box-inherit block'>
+        <Helmet>
+          <meta property="og:title" content="kip2 Home Page"/>
+          <meta property="og:description" content="kip2がいろいろなものを虫干しするホームページ"/>
+          <meta property="og:image" content="/img/myimage.png"/>
+          <meta property="og:url" content="https://www.kip2.dev"/>
+          <meta property="og:type" content="wewbsite"/>
+        </Helmet>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Top />}/>
+            <Route path="/products" element={<Products />}/>
+            <Route path="/products/:id" element={<ProductDetail />} />
+            <Route path="/about" element={<About />}/>
+            <Route path="/about/history/:id" element={<HistoryDetail />} />
+            <Route path="/favorite" element={<Favorite />}/>
+            <Route path="/favorite/:id" element={<FavoriteDetail />}/>
+            <Route path="/influenced" element={<Influenced />}/>
+            <Route path="/influenced/:id" element={<InfluencedDetail />}/>
+            <Route path="/about-site" element={<AboutSite />}/>
+            <Route path="*" element={<NotFoundPage />}/>
+          </Routes>
+        </Router>
+      </div>
+    </HelmetProvider>
   )
 }
 
