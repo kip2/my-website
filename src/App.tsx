02702c73828e9e797a9ac6c386be71549d24f5c4
kip2 <tqa1no4@gmail.com>
2024-05-25 19:39:12 +0900

    Fix ogp tag

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,11 @@ const App = () => {
           <meta property="og:title" content="kip2 Home Page"/>
           <meta property="og:description" content="kip2がいろいろなものを虫干しするホームページ"/>
           <meta property="og:image" content="https://www.kip2.dev/img/myimage.png" />
-          <meta property="og:url" content="https://www.kip2.dev"/>
+          <meta property="og:url" content="https://www.kip2.dev/"/>
           <meta property="og:type" content="website"/>
 
           {/* Twitter Card meta tags */}
-          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@_kip2" />
           <meta name="twitter:title" content="kip2 Home Page" />
           <meta name="twitter:description" content="kip2がいろいろなものを虫干しするホームページ" />
